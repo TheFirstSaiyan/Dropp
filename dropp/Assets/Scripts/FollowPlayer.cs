fd8d27a4fa1edded9c73ad92080a1624d1c0a735
TheFirstSaiyan <19hari98@gmail.com>
2018-06-08 19:33:46 +0530

    first commit

---
+++ dropp/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = GameObject.Find("player").transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = GameObject.Find("player").transform.position - offset;
+	}
+}
---
+++ dropp/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+            public Rigidbody rb;
+            private Vector2 startPos;
+            private Vector2 endPos;
+            private float force=150;
+            private float maxDragDistance = 65f;
+	        void Start ()
+            {
+		
+	        }
+	
+	        void Update ()
+            {
+
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    startPos = Input.mousePosition;
+
+                }
+        
+                if(Input.GetMouseButtonUp(0) && Input.mousePosition.y> startPos.y + 5)
+                {
+            
+                        endPos = Input.mousePosition;
+            /* if (Vector3.Distance(startPos, endPos) <= maxDragDistance)
+                 ThrowBall(0);
+             else {
+                 ThrowBall(1);
+             }*/
+            ThrowBall();
+                }
+	        }
+
+            private void ThrowBall()
+            {
+                Vector2 direction = endPos - startPos;
+        //if (flag == 0)
+          //  force = Vector3.Distance(startPos, endPos);
+       // else
+           // force = maxDragDistance;
+                Debug.Log(force);
+                rb.AddForce(direction/direction.magnitude * force * Time.deltaTime, ForceMode.Impulse);
+            }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("safe"))
+        {
+            rb.isKinematic = true;
+            rb.isKinematic = false;
+        }
+
+
+        }
+
+    }
