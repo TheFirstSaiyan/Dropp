78bddd64dd9dffecc9cc03e3e52883ee6d5b9e1e
TheFirstSaiyan <19hari98@gmail.com>
2018-06-12 17:47:37 +0530

    changed gameplay

---
+++ dropp/Assets/ObstacleManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.ComponentModel;
+using UnityEngine.Analytics;
+
+public class ObstacleManager : MonoBehaviour {
+
+
+	public GameObject blockPrefab;
+	public Transform player;
+	private List<GameObject> blocks;
+	private List<Vector3> blockLocations;
+	private const float XGAP = 6f;
+	private const float YGAP= 1f;
+	private float timePassed=0f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		blocks = new List<GameObject> ();
+		blockLocations = new List<Vector3> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+			timePassed += Time.deltaTime;
+			if(timePassed >=0.8f)
+			{
+			if (blocks.Count == 0) {
+				GameObject GO = Instantiate (blockPrefab, new Vector3 (player.position.x + 5f, Random.Range (3.2f, 12.8f), player.position.z), Quaternion.identity);
+				blocks.Add (GO);
+				blockLocations.Add (GO.transform.position);
+			} else 
+			{
+				float Y = Mathf.Clamp (player.position.y, 3.2f, 12.8f);
+				//GameObject GO = Instantiate (blockPrefab, new Vector3 (player.position.x + Random.Range(3,XGAP),Y, player.position.z), Quaternion.identity);
+
+				GameObject GO = Instantiate (blockPrefab, new Vector3 (player.position.x + Random.Range(3,XGAP), Random.Range (player.position.y,8f), player.position.z), Quaternion.identity);
+				blocks.Add (GO);
+				blockLocations.Add (GO.transform.position);
+
+
+			}
+				
+
+
+			for (int i = blocks.Count-1; i >=0; i--) 
+			{
+			
+				if (player.position.x - blockLocations [i].x > 8f)
+				{
+
+					Destroy (blocks [i]);
+					blocks.RemoveAt (i);
+					blockLocations.RemoveAt (i);
+				}
+					timePassed=0f;
+			
+			}
+			}
+	}
+}
--- dropp/Assets/Scripts/FollowPlayer.cs
+++ dropp/Assets/Scripts/FollowPlayer.cs
@@ -6,13 +6,16 @@ public class FollowPlayer : MonoBehaviour {
 
 
     private Vector3 offset;
+	private float startY;
 	// Use this for initialization
 	void Start () {
         offset = GameObject.Find("player").transform.position - transform.position;
+		startY = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.position = GameObject.Find("player").transform.position - offset;
+		transform.position = new Vector3 (transform.position.x,startY,transform.position.z);
 	}
 }
